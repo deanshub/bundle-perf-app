a31dd4c3a52e757db6191e8bfca19e00128d44ba
Dean Shub <deanshub@gmail.com>
2023-07-30 10:39:20 +0300

    remove in-it

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withInItStats = require('next-in-it-stats/cjs')({
-    legacy: true,
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 }
 
-module.exports = withInItStats(nextConfig)
+module.exports = nextConfig
